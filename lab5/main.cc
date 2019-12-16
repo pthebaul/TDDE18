99d799e3a187310a9385c6698a9ba3b85b57e436
pthebaul <pthebaul@etud.insa-toulouse.fr>
2019-12-16 19:19:33 +0100

    lab5: fin avant démonstration

--- lab5/main.cc
+++ lab5/main.cc
@@ -7,51 +7,50 @@
 #include <iterator>
 #include <algorithm>
 #include <cctype>
+#include <sstream>
+#include <cmath>
+#include <iomanip>
 
 using namespace std;
 
 int main(int argc, char* argv[])
 {
-    // Get filename from command-line arguments
+    // Handling command-line arguments
     
-    bool syntax_error{false};
-    int return_code{};
-    if (argc <= 1)
+    string filename{""};
+    char option;
+    try
     {
-	syntax_error = true;
-	cout << "Error: No arguments given." << endl;
-	return_code = 1;
+	if (argc <= 2)
+	{
+	    cerr << "Error: Not enough arguments." << endl;
+	    throw 1;
+	}
+	else
+	{
+	    filename = argv[1];
+	    option = argv[2][1];
+	    
+	    if ((argv[2][0] != '-') || ((option != 'a') && (option != 'f') && (option != 'o')))
+	    {
+		cerr << "Error: Invalid second argument." << endl;
+		throw 2;
+	    }
+	    else if (((option != 'o') && (argc > 3)) || (argc > 4))
+	    {
+		cerr << "Error: Too many arguments." << endl;
+		throw 3;
+	    }
+	}
     }
-    else if (argc <= 2)
+    catch (int const& error_code)
     {
-	syntax_error = true;
-	cout << "Error: Second argument missing." << endl;
-	return_code = 2;
-    }
-    else if ((strcmp(argv[2], "-a") != 0)
-	     && (strcmp(argv[2], "-f") != 0)
-	     && (strcmp(argv[2], "-o") != 0))
-    {
-	syntax_error = true;
-	cout << "Error: Invalid second argument." << endl;
-	return_code = 3;
-    }
-    else if (((strcmp(argv[2], "-o") != 0) && (argc > 3))
-	     || ((strcmp(argv[2], "-o") == 0) && (argc > 4)))
-    {
-	syntax_error = true;
-	cout << "Error: Too many arguments." << endl;
-	return_code = 4;
-    }
-
-    if (syntax_error)
-    {
-	cout << "Usage: ./a.out FILE [-a] [-f] [-o N]" << endl;
-	return return_code;
+	cerr << "Usage: ./a.out FILE [-a] [-f] [-o N]" << endl;
+	return error_code;
     }
     
-    int line_size{};
-    if (strcmp(argv[2], "-o") == 0)
+    string::size_type line_size{0};
+    if (option == 'o')
     {
 	try
 	{
@@ -63,115 +62,169 @@ int main(int argc, char* argv[])
 	}
 	catch(invalid_argument& e)
 	{
-	    cout << "N must be an integer" << endl;
-	    cout << "Usage: ./a.out FILE [-a] [-f] [-o N]" << endl;
+	    cerr << "Error: N must be an integer" << endl;
+	    return 4;
 	}
 	catch(out_of_range& e)
 	{
-	    cout << "N must be a not-so-big strictly positive integer" << endl;
-	    cout << "Usage: ./a.out FILE [-a] [-f] [-o N]" << endl;
+	    cerr << "Error: N must be a not-so-big strictly positive integer" << endl;
+	    return 5;
 	}
     }
        
-    // Open file with filename (handle errors)
-
-    string filename{argv[1]};
+// Opening file and handling eventual errors
     ifstream ifs{filename};
 
     if (!ifs.is_open())
     {
-	cout << "Error: Could not open file." << endl;
-	return 5;
+	cerr << "Error: Could not open file." << endl;
+	return 6;
     }
     
-    // Put all potential words in a container ( >> string is good)
-  
-    vector<string> potential_words{istream_iterator<string>(ifs), istream_iterator<string>()};
+// Putting all potential words in a vector
+    vector<string> words{istream_iterator<string>(ifs), istream_iterator<string>()};
+
     
-    // Clean words : leading, trailing, 's
-    vector<string> cleaned_words{};
+// Cleaning words : leading chars, trailing chars, trailing 's
     auto cleaning = [](string s)
-    {
-	string::size_type fno_pos{s.find_first_not_of("\"'(")};
-	s = s.substr(fno_pos);
-	string::size_type lno_pos{s.find_last_not_of("!?;,:.\"')")};
-	s = s.substr(0, lno_pos+1);
-	if ((s.size() >= 2) && (s.substr(s.size() - 2) == "'s"))
 	{
-	    s = s.substr(0, s.size() - 2);
-	}
-	return s;
-    };
-
-    transform(potential_words.begin(), potential_words.end(), back_inserter(cleaned_words), cleaning);
+	    string::size_type fno_pos{s.find_first_not_of("\"'(")};
+	    if (fno_pos == string::npos)
+	    {
+		fno_pos = s.size();
+	    }
+	    string::size_type lno_pos{s.find_last_not_of("!?;,:.\"')")};
+	    s = s.substr(fno_pos, lno_pos+1);
+	    if ((s.size() >= 2) && (s.substr(s.size() - 2) == "'s"))
+	    {
+		s = s.substr(0, s.size() - 2);
+	    }
+	    return s;
+	};
+    transform(words.begin(), words.end(), words.begin(), cleaning);
     
     
-    // Keep only valid words : only letters and hyphens, no leading, trailing or consecutive hyphens, no less than 3 characters
-    vector<string> valid_words{};
-    auto is_valid = [](string s)
-    {
-	bool valid{true};
-	if (s.size() < 3)
+// Keeping only valid words : no less than 3 characters, only letters and hyphens, no leading, trailing or consecutive hyphens
+    auto is_invalid = [](string s)
 	{
-	    valid = false;
-	}
-	else if (s.find_first_not_of("qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM-") != string::npos)
-	{
-	    valid = false;
-	}
-	else if (s.find_first_of("--") != string::npos)
-	{
-	    valid = false;
-	}
-	else if (s.front() == '-' || s.back() == '-')
-	{
-	    valid = false;
-	}
-	return valid;
-    };
-    copy_if(cleaned_words.begin(), cleaned_words.end(), back_inserter(valid_words), is_valid);
+	    return (s.size() < 3)
+	    || (s.find_first_not_of("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-") != string::npos)
+	    || (s.front() == '-') || (s.back() == '-')
+	    || (s.find("--") != string::npos);
+	};
 
+    // remove_if does not remove elements but shift them, so we have to erase them
+    words.erase(remove_if(words.begin(), words.end(), is_invalid), words.end());
+    
+// Putting valid words in lowercase
     auto str_lower = [](string s)
-    {
-	string result{s};
-	transform(s.begin(), s.end(), result.begin(), [](char c) { return tolower(c); });
-	return result;
-    };
-    transform(valid_words.begin(), valid_words.end(), valid_words.begin(), str_lower);
+	{
+	    string result{s};
+	    transform(s.begin(), s.end(), result.begin(), [](char c) { return tolower(c); });
+	    return result;
+	};
+    transform(words.begin(), words.end(), words.begin(), str_lower);
     
-    // -a   : Sort alphabetically (ascending), format by left-aligned word, right-aligned frequency
-    // -f   : Sort by decreasing frequency,    format by right-aligned word, right-aligned frequency
-    // -o N : Sort by occurence in the file, with newlines to prevent lines getting bigger than N
-
-    if (strcmp(argv[2], "-o") != 0)
+// -a   : Sort alphabetically (ascending), format by left-aligned word, right-aligned frequency
+// -f   : Sort by decreasing frequency,    format by right-aligned word, right-aligned frequency
+// -o N : Keep the original order, with newlines to prevent lines getting bigger than N chars
+    if (words.empty())
     {
-	map<string, int> m;
-	auto add_to_m = [&m](string s)
+	cerr << "Warning: No valid words found" << endl;
+    }
+    else
+    {
+	// Finding the longest word
+	auto compare_by_length = [](string const& l, string const& r)
+	    {
+		return l.size() < r.size();
+	    };
+	string::size_type longest_word_length{(*max_element(words.begin(), words.end(), compare_by_length)).size()};
+	
+	if (option != 'o')
 	{
-	    m[s]++;
-	};
-	for_each(valid_words.begin(), valid_words.end(), add_to_m);
-
-	vector<pair<string, int>> vect{};
-	copy(m.begin(), m.end(), back_inserter(vect));
-
-	auto compare = [](pair<string, int> const& l, pair<string, int> const& r)
+	    // Counting occurences of words by putting them in a map
+	    map<string, int> word_frequency_map;
+	    auto add_to_word_frequency_map = [&word_frequency_map](string s)
+		{
+		    word_frequency_map[s]++;
+		};
+	    for_each(words.begin(), words.end(), add_to_word_frequency_map);
+	    
+	    // Calculating the length of the biggest number
+	    
+	    int longest_number_length{1 + static_cast<int>(log((--word_frequency_map.end())->second)/log(10))};
+	    if (option == 'a')
+	    {
+		// Formatting according to requirements
+		auto format = [longest_word_length, longest_number_length](pair<string, int> const& l)
+		    {
+			ostringstream oss{};
+			oss << left << setw(longest_word_length) << l.first;
+			oss << "  ";
+			oss << right << setw(longest_number_length) << l.second;
+			oss << endl;
+			
+			return oss.str();
+		    };
+		transform(word_frequency_map.begin(), word_frequency_map.end(), ostream_iterator<string>(cout), format);
+	    }
+	    else
+	    {
+		// Putting every word/frequency pair in a vector
+		vector<pair<string, int>> word_frequency_vect{word_frequency_map.begin(), word_frequency_map.end()};
+		
+		// Sorting the vector by frequency
+		auto compare_by_frequency = [](pair<string, int> const& l, pair<string, int> const& r)
+		    {
+			return l.second > r.second;
+		    };
+		sort(word_frequency_vect.begin(), word_frequency_vect.end(), compare_by_frequency);
+		
+		// Formatting according to requirements
+		auto format = [longest_word_length, longest_number_length](pair<string, int> const& l)
+		    {
+			ostringstream oss{};
+			oss << right;
+			oss << setw(longest_word_length) << l.first;
+			oss << "  ";
+			oss << setw(longest_number_length) << l.second;
+			oss << endl;
+			
+			return oss.str();
+		    };
+		transform(word_frequency_vect.begin(), word_frequency_vect.end(), ostream_iterator<string>(cout), format);
+	    }
+	}
+	else
 	{
-	    return l.second > r.second;
-	};
-	sort(vect.begin(), vect.end(), compare);	    
-	
-
-	
-	auto print = [](pair<string, int> x)
-	{
-	    cout << x.first << ':' << x.second << endl;
-	};
-	
-	for_each(vect.begin(), vect.end(), print);
-	cout << endl;
-	for_each(m.begin(), m.end(), print);
-	
+	    if (longest_word_length >= line_size)
+	    {
+		cerr << "Error: There is a words longer than N characters" << endl;
+		return 7;
+	    }
+	    
+	    string::size_type char_count{0};
+	    auto format = [&char_count, line_size](string s)
+		{
+		    // If printing the word would exceed the limit
+		    if ((char_count + 1 + s.size()) >= line_size)
+		    {
+			cout << endl;
+			char_count = 0;
+		    }
+		    // Else if it is not the first word
+		    else if (char_count > 0)
+		    {
+			cout << " ";
+			char_count++;
+		    }
+		    cout << s;
+		    char_count += s.size();
+		};
+	    for_each(words.begin(), words.end(), format);
+	    cout << endl;
+	}
     }
     return 0;
 }
